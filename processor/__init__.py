f8f6dc8fb3e112f48142496be362cc75c85c7524
Rahul B <rahulbot@gmail.com>
2021-01-14 20:52:42 -0500

    integrate with Sentry for centralized error logging

--- processor/__init__.py
+++ processor/__init__.py
@@ -1,8 +1,10 @@
 import os
 import logging
+import sys
 from dotenv import load_dotenv
 import mediacloud.api
 from flask import Flask
+from sentry_sdk import init, capture_message
 
 load_dotenv()  # load config from .env file (local) or env vars (production)
 
@@ -15,9 +17,24 @@ logger.info("-------------------------------------------------------------------
 logger.info("Starting up Feminicide MC Story Processor")
 
 # read in environment variables
-MC_API_KEY = os.environ['MC_API_KEY'] # sensitive, so don't log it
-BROKER_URL = os.environ['BROKER_URL']
+MC_API_KEY = os.environ.get('MC_API_KEY', None)  # sensitive, so don't log it
+if MC_API_KEY is None:
+    logger.error("No MC_API_KEY env var specified. Pathetically refusing to start!")
+    sys.exit(1)
+
+BROKER_URL = os.environ.get('BROKER_URL', None)
+if BROKER_URL is None:
+    logger.error("No BROKER_URL env var specified. Pathetically refusing to start!")
+    sys.exit(1)
 logger.info("BROKER_URL: {}".format(BROKER_URL))
+
+SENTRY_DSN = os.environ.get('SENTRY_DSN', None)  # optional
+if SENTRY_DSN:
+    init(dsn=SENTRY_DSN)
+    capture_message("Initializing")
+    logger.info("SENTRY_DSN: {}".format(SENTRY_DSN))
+else:
+    logger.info("Not logging errors to Sentry")
 
 
 def get_mc_client():
