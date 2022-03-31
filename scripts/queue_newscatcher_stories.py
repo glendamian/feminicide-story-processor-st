51e5d1bfba386e25cc2199ca0ba1f4e380f4839a
Rahul Bhargava <rahulbot@gmail.com>
2022-03-30 21:55:30 -0400

    integration test fixes
    paging, rate limiting, logging

--- scripts/queue_newscatcher_stories.py
+++ scripts/queue_newscatcher_stories.py
@@ -1,10 +1,12 @@
 import logging
 import math
 from typing import List, Dict
+import time
 import mcmetadata as metadata
 import datetime as dt
 from prefect import Flow, task, Parameter
 from newscatcherapi import NewsCatcherApiClient
+from prefect.executors import LocalDaskExecutor
 
 import processor
 import processor.database.projects_db as projects_db
@@ -14,6 +16,9 @@ import scripts.tasks as prefect_tasks
 
 PAGE_SIZE = 100
 DAY_WINDOW = 3
+WORKER_COUNT = 16
+MAX_CALLS_PER_SEC = 5
+DELAY_SECS = 1 / MAX_CALLS_PER_SEC
 
 newscatcherapi = NewsCatcherApiClient(x_api_key=processor.NEWSCATCHER_API_KEY)
 
@@ -30,43 +35,44 @@ def load_projects_task() -> List[Dict]:
 def _fetch_results(project: Dict, start_date, end_date, page: int = 1) -> Dict:
     return newscatcherapi.get_search(
         q=project['search_terms'],
-        lang=project['language'],
+        lang=project['language'],  # TODO: split this by comma and trim
         countries=project['country'],
         page_size=PAGE_SIZE,
         from_=start_date.strftime("%Y-%m-%d"),
         to_=end_date.strftime("%Y-%m-%d"),
+        page=page
     )
 
 
 @task(name='fetch_project_stories')
 def fetch_project_stories_task(project_list: Dict, data_source: str) -> List[Dict]:
     combined_stories = []
+    end_date = dt.date.today()
+    start_date = end_date - dt.timedelta(days=DAY_WINDOW)
     for p in project_list:
         project_stories = []
         valid_stories = 0
         history = projects_db.get_history(p['id'])
-        end_date = dt.date.today()
-        start_date = end_date - dt.timedelta(days=DAY_WINDOW)
         page_number = 1
         current_page = _fetch_results(p, start_date, end_date, page_number)
         total_hits = current_page['total_hits']
         logger.info("Project {}/{} - {} total stories".format(p['id'], p['title'], total_hits))
         if total_hits > 0:
-            page_count = math.ceil(total_hits / current_page)
-            keep_going = True;
+            page_count = math.ceil(total_hits / PAGE_SIZE)
+            keep_going = True
             while keep_going:
+                logger.debug("  {} - page {}: {} stories".format(p['id'], page_number, len(current_page['articles'])))
                 for item in current_page['articles']:
-                    logger.info("  {} - page {}: {} stories".format(p['id'], page_number, len(current_page['articles'])))
                     # maybe stop when we hit a url we've processed already
                     real_url = item['link']
                     if history.last_url == real_url:
                         logger.info("  Found last_url on {}, skipping the rest".format(p['id']))
                         keep_going = False
-                        break # out of the for loop of all articles on page, back to while "more pages"
+                        break  # out of the for loop of all articles on page, back to while "more pages"
                     # story was published more recently than latest one we saw, so process it
                     info = dict(
                         url=real_url,
-                        source_publish_date=item['published'],
+                        source_publish_date=item['published_date'],
                         title=item['title'],
                         source=data_source,
                         project_id=p['id'],
@@ -74,13 +80,17 @@ def fetch_project_stories_task(project_list: Dict, data_source: str) -> List[Dic
                         authors=item['authors'],
                         media_url=metadata.domains.from_url(real_url),
                         media_name=metadata.domains.from_url(real_url)
-                        # too bad there isn't somewher we can store the `id` (string)
+                        # too bad there isn't somewhere we can store the `id` (string)
                     )
                     project_stories.append(info)
                     valid_stories += 1
                 if keep_going:  # check after page is processed
-                    keep_going = page_number <= page_count
-                    _fetch_results(p, start_date, end_date, page_number)
+                    keep_going = page_number < page_count
+                    if keep_going:
+                        page_number += 1
+                        time.sleep(DELAY_SECS)
+                        current_page = _fetch_results(p, start_date, end_date, page_number)
+                        # stay below rate limiting
             logger.info("  project {} - {} valid stories (after {})".format(p['id'], valid_stories,
                                                                             history.last_publish_date))
             combined_stories += project_stories
@@ -97,11 +107,12 @@ if __name__ == '__main__':
     download_models()
 
     with Flow("story-processor") as flow:
-        #flow.executor = LocalDaskExecutor(scheduler="threads", num_workers=16)  # execute `map` calls in parallel
+        if WORKER_COUNT > 1:
+            flow.executor = LocalDaskExecutor(scheduler="threads", num_workers=WORKER_COUNT)  # execute `map` calls in parallel
         data_source_name = Parameter("data_source", default="")
         # 1. list all the project we need to work on
         projects_list = load_projects_task()
-        # 2. fetch all the urls from for each project from newscatcher
+        # 2. fetch all the urls from for each project from newscatcher (not mapped, so we can respect rate limiting)
         all_stories = fetch_project_stories_task(projects_list, data_source_name)
         # 3. fetch webpage text and parse all the stories (will happen in parallel by story)
         stories_with_text = prefect_tasks.fetch_text_task.map(all_stories)
