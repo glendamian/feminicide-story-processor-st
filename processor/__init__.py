5bafecd85bf56423764fd8fd82dc96378be99b14
Rahul Bhargava <rahulbot@gmail.com>
2023-01-06 10:59:04 -0500

    version bump

--- processor/__init__.py
+++ processor/__init__.py
@@ -11,7 +11,7 @@ from sentry_sdk import init
 from typing import Dict
 from sqlalchemy import create_engine
 
-VERSION = "3.0.5"
+VERSION = "3.0.6"
 SOURCE_GOOGLE_ALERTS = "google-alerts"
 SOURCE_MEDIA_CLOUD = "media-cloud"
 SOURCE_NEWSCATCHER = "newscatcher"
