11daba2be59428e1e6863b95b4690a68cedda643
Rahul Bhargava <rahulbot@gmail.com>
2023-02-18 14:16:23 -0500

    redo graph endpoints to be more consistent

--- processor/server.py
+++ processor/server.py
@@ -26,39 +26,87 @@ def home():
     return render_template('home.html', projects=projects, version=VERSION)
 
 
-@app.route("/posted-platform-history", methods=['GET'])
-def posted_platform_history():
-    # show overall ingest over last month
-    data_for_graph = _prep_for_stacked_graph(
-        [stories_db.stories_by_posted_day(platform=p) for p in PLATFORMS],
-        PLATFORMS)
-    return jsonify(data_for_graph)
-
-
-@app.route("/processed-platform-history", methods=['GET'])
-def processed_platform_history():
-    # show overall ingest over last month
-    data_for_graph = _prep_for_stacked_graph(
-        [stories_db.stories_by_processed_day(platform=p) for p in PLATFORMS],
-        PLATFORMS)
-    return jsonify(data_for_graph)
-
-
-@app.route("/processed-result-history", methods=['GET'])
-def processed_result_history():
-    # show stories above threshold over last month
-    data = _prep_for_stacked_graph(
-        [stories_db.stories_by_processed_day(above_threshold=True),
-         stories_db.stories_by_processed_day(above_threshold=False)],
-        ['above threshold', 'below threshold']
-    )
-    return jsonify(data)
-
-
-@app.route("/update-config", methods=['POST'])
+@app.route("/api/update-config", methods=['POST'])
 def update_config():
     config = load_project_list(force_reload=True)
     return jsonify(config)
+
+
+@app.route("/projects/<project_id_str>", methods=['GET'])
+def a_project(project_id_str: int):
+    # pull out the project info
+    project_id = int(project_id_str)
+    project = [p for p in load_project_list(download_if_missing=True) if p['id'] == project_id][0]
+    # show overall ingest over last two weeks
+    data_for_graph = _prep_for_stacked_graph(
+        [stories_db.stories_by_published_day(platform=p, project_id=project_id, limit=30) for p in PLATFORMS],
+        PLATFORMS)
+    # show some recent story results
+    stories_above = stories_db.recent_stories(project_id, True)
+    stories_below = stories_db.recent_stories(project_id, False)
+    # some other stats
+    unposted_above_story_count = stories_db.unposted_above_story_count(project_id)
+    posted_above_story_count = stories_db.posted_above_story_count(project_id)
+    below_story_count = stories_db.below_story_count(project_id)
+    try:
+        above_threshold_pct = 100 * (unposted_above_story_count + posted_above_story_count) / below_story_count
+    except ZeroDivisionError:
+        above_threshold_pct = 100
+    # render it all
+    return render_template('project.html',
+                           ingest_data=data_for_graph,
+                           unposted_above_story_count=unposted_above_story_count,
+                           above_threshold_pct=above_threshold_pct,
+                           posted_above_story_count=posted_above_story_count,
+                           below_story_count=below_story_count,
+                           project=project,
+                           stories_above=stories_above,
+                           stories_below=stories_below,
+                           )
+
+
+def _posted_platform_history(project_id: int = None) -> List[Dict]:
+    return _prep_for_stacked_graph(
+        [stories_db.stories_by_posted_day(project_id=project_id, platform=p) for p in PLATFORMS],
+        PLATFORMS)
+@app.route("/api/posted-platform-history", methods=['GET'])
+def posted_platform_history():
+    return jsonify(_posted_platform_history())
+@app.route("/api/projects/<project_id_str>/posted-platform-history", methods=['GET'])
+def project_posted_platform_history(project_id_str: str):
+    return jsonify(_posted_platform_history(int(project_id_str)))
+
+
+def _processed_platform_history(project_id: int = None) -> List[Dict]:
+    return _prep_for_stacked_graph(
+        [stories_db.stories_by_processed_day(project_id=project_id, platform=p) for p in PLATFORMS],
+        PLATFORMS)
+@app.route("/api/processed-platform-history", methods=['GET'])
+def processed_platform_history():
+    return jsonify(_processed_platform_history())
+@app.route("/api/projects/<project_id_str>/processed-platform-history", methods=['GET'])
+def project_processed_platform_history(project_id_str: str):
+    return jsonify(_processed_platform_history(int(int(project_id_str))))
+
+
+def _processed_result_history(project_id: int = None) -> List[Dict]:
+    return _prep_for_stacked_graph(
+        [stories_db.stories_by_processed_day(project_id=project_id, above_threshold=True),
+         stories_db.stories_by_processed_day(project_id=project_id, above_threshold=False)],
+        ['above threshold', 'below threshold'])
+@app.route("/api/processed-result-history", methods=['GET'])
+def processed_result_history():
+    return jsonify(_processed_result_history())
+@app.route("/api/projects/<project_id_str>/processed-result-history", methods=['GET'])
+def project_processed_result_history(project_id_str: str):
+    return jsonify(_processed_result_history(int(project_id_str)))
+
+
+@app.route("/api/projects/<project_id_str>/binned-model-scores", methods=['GET'])
+def project_model_scores(project_id_str):
+    project_id = int(project_id_str)
+    data = stories_db.project_binned_model_scores(project_id)
+    return jsonify(data)
 
 
 def _prep_for_stacked_graph(counts: List[List], names: List[str]) -> List[Dict]:
@@ -73,84 +121,6 @@ def _prep_for_stacked_graph(counts: List[List], names: List[str]) -> List[Dict]:
                 count=series[d] if d in series else 0
             ))
     return stories_by_day_data
-
-
-@app.route("/projects/<project_id_str>/processed-by-day", methods=['GET'])
-def project_processed_by_day(project_id_str):
-    project_id = int(project_id_str)
-    data = _prep_for_stacked_graph(
-        [stories_db.stories_by_processed_day(project_id, above_threshold=True),
-         stories_db.stories_by_processed_day(project_id, above_threshold=False)],
-        ['above threshold', 'below threshold']
-    )
-    return jsonify(data)
-
-
-@app.route("/projects/<project_id_str>/published-by-day", methods=['GET'])
-def project_published_by_day(project_id_str):
-    project_id = int(project_id_str)
-    data = _prep_for_stacked_graph(
-        [stories_db.stories_by_published_day(project_id=project_id, above_threshold=True),
-         stories_db.stories_by_published_day(project_id=project_id, above_threshold=False)],
-        ['above threshold', 'below threshold']
-    )
-    return jsonify(data)
-
-
-@app.route("/projects/<project_id_str>/posted-by-day", methods=['GET'])
-def project_posted_by_day(project_id_str):
-    project_id = int(project_id_str)
-    data = _prep_for_stacked_graph(
-        [stories_db.stories_by_processed_day(project_id, above_threshold=True, is_posted=True),
-         stories_db.stories_by_processed_day(project_id, above_threshold=True, is_posted=False)],
-        ['sent to main server', 'not sent to main server']
-    )
-    return jsonify(data)
-
-
-@app.route("/projects/<project_id_str>/binned-model-scores", methods=['GET'])
-def project_model_scores(project_id_str):
-    project_id = int(project_id_str)
-    data = stories_db.project_binned_model_scores(project_id)
-    return jsonify(data)
-
-
-@app.route("/projects/<project_id_str>", methods=['GET'])
-def a_project(project_id_str):
-    project_id = int(project_id_str)
-    # pull out the project info
-    project = [p for p in load_project_list(download_if_missing=True) if p['id'] == project_id][0]
-
-    # show overall ingest over last two weeks
-    data_for_graph = _prep_for_stacked_graph([stories_db.stories_by_published_day(platform=p, project_id=project_id, limit=30)
-                                              for p in PLATFORMS], PLATFORMS)
-
-    # show some recent story results
-    stories_above = stories_db.recent_stories(project_id, True)
-    stories_below = stories_db.recent_stories(project_id, False)
-    story_lookup = {}
-
-    # some other stats
-    unposted_above_story_count = stories_db.unposted_above_story_count(project_id)
-    posted_above_story_count = stories_db.posted_above_story_count(project_id)
-    below_story_count = stories_db.below_story_count(project_id)
-    try:
-        above_threshold_pct = 100 * (unposted_above_story_count + posted_above_story_count) / below_story_count
-    except ZeroDivisionError:
-        above_threshold_pct = 100
-
-    # render it all
-    return render_template('project.html',
-                           ingest_data=data_for_graph,
-                           unposted_above_story_count=unposted_above_story_count,
-                           above_threshold_pct=above_threshold_pct,
-                           posted_above_story_count=posted_above_story_count,
-                           below_story_count=below_story_count,
-                           project=project,
-                           stories_above=stories_above,
-                           stories_below=stories_below,
-                           story_lookup=story_lookup
-                           )
 
 
 if __name__ == "__main__":
